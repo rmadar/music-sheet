a023155580c61841bb278ddad968a66c4ddfeffe
rmadar <romain.madar@gmail.com>
2021-04-01 13:10:10 +0200

    Re-organizing some code

--- exercise-reading/example_random_score.py
+++ exercise-reading/example_random_score.py
@@ -1,11 +1,116 @@
 import sys
 sys.path.append("../src")
+import numpy as np
 import score_creator as sc
+
+
+# Dictionnaire key=nombre de temps, value=code dans lilypond
+durees = {
+    4   : '1'  , # ronde
+    2   : '2'  , # blanche
+    3   : '2.' , # blanche pointee
+    1   : '4'  , # noire
+    1.50: '4.' , # noire pointee
+    0.50: '8'  , # croche
+    0.75: '8.' , # croche pointee
+    0.25: '16' , # double-croche
+}
+
+# All notes
+notes = [
+    'e,' ,
+    'f,' ,
+    'g,' ,
+    'a,' , # la
+    'b,' , # si
+    'c'  , # do
+    'd'  , # re
+    'e'  , # mi
+    'f'  , # fa
+    'g'  , # sol
+    'a'  , # la
+    'b'  , # si
+    'c\'',
+    'd\'',
+    'e\'', 
+    'f\'', 
+    'g\'', 
+    'a\'', 
+    'b\'',
+]
+
+# Genere un temps
+def un_temps(nNoteMax=4, noirePointee=True, triolet=False, sextuplet=False):
+    '''
+    Cette function retourne le placement rythmique d'un temps, selon
+    le nombre de notes aleatoire jouees dans ce temps. Ici, un silence
+    est consideree comme une "note". Le niveau de difficulte peut etre 
+    parametrise, avec le nombre max de note par temps (4, double-croche),
+    la presence de noire pointee ou de triolets (TODO), ou de sextuplets (TODO).
+
+    Return:
+    -------
+      * liste de durees dont la somme fait 1. La taille de cette list
+        depend de la segmentation du temps.
+
+    Arguments:
+    ----------
+      * nNoteMax      [int]: nombre maximum de notes par temps 
+      * noirePointee [bool]: inclure ou non des pattern avec 
+                             noires pointees + double-croche.
+      * triolet      [bool]: inclure des triolets   TO-DO
+      * sextuplet    [bool]: inclure des sextuplets TO-DO
+    '''
+
+    if nNoteMax>4:
+        raise NameError("Cant play more than four note")
+    
+    # Number of "notes" between 1 and 4
+    nNotes = np.random.randint(low=1, high=nNoteMax+1)
+    
+    # Container for the result, valant une noire par defaut
+    tps = [durees[1]]
+
+    # Quatre notes dans le tps, necessairement 4 double-croches
+    if nNotes == 4 :
+        tps = [durees[0.25]]*4
+        
+    # Trois notes: 1 croche + 2 double
+    if nNotes == 3 :
+        x = rnd.random()
+        if x < 0.33:
+            tps = [durees[0.25], durees[0.25], durees[0.50]]
+        elif x < 0.66:
+            tps = [durees[0.25], durees[0.50], durees[0.25]]
+        else:
+            tps = [durees[0.50], durees[0.25], durees[0.25]]
+            
+    # Deux notes: deux croches ou une croche pointee + double-croche
+    if nNotes == 2:
+        
+        # Deux croches
+        tps = [durees[0.50], durees[0.50]]
+
+        # croche pointee + double-croche
+        if noirePointee and rnd.random() < 0.5:
+            if rnd.random() < 0.5:
+                tps = [durees[0.25], durees[0.75]]
+            else:
+                tps = [durees[0.75], durees[0.25]]
+        
+    # Deux double croches: 
+    if nNotes == 1:
+        tps = [durees[1]]
+
+    # Return the result
+    return tps
+
+
 
 # Generation de 100 mesure de note et rythme aleatoire.
 Nmeas  = 100
-piches = [sc.notes[i] for i in sc.rnd.randint(low=0, high=len(sc.notes)-5, size=100*4)]
-times  = [t for i in range(4*Nmeas) for t in sc.un_temps(2, False)] 
+piches = [notes[i] for i in np.random.randint(low=0, high=len(notes)-5, size=100*4)]
+times  = [t for i in range(4*Nmeas) for t in un_temps(2, False)] 
 notes  = " ".join(["{}{}".format(p, t) for p, t in zip(piches, times)])
 
 # Creer une portee a partir de ces notes
--- exercise-rythm/example_rythm_patterns.py
+++ exercise-rythm/example_rythm_patterns.py
@@ -1,8 +1,70 @@
 import sys
 sys.path.append("../src")
+import numpy as np
 import score_creator as sc
 
-pattern = sc.rythm_pattern_16th
+# Store all possibilities from 16-th notes
+pattern = {
+
+    # 1 note
+    '1 note - A': ['4'                         ],
+    '1 note - B': ['r16', '16'  , 'r16', 'r16' ],
+    '1 note - C': ['r8' , '8'                  ],
+    '1 note - D': ['r16', 'r16' , 'r16', '16'  ],
+
+    # 2 notes
+    '2 notes - A ': ['r8' , '16' , '16'        ],
+    '2 notes - B1': ['16' , '16' , 'r8'        ],
+    '2 notes - B2': ['16' , '8.'               ],
+    '2 notes - B3': ['16' , '8'  , 'r16'       ],
+    '2 notes - C1': ['r16', '16' , '16' , 'r16'],
+    '2 notes - C2': ['r16', '16' , '8'  ,      ],
+    '2 notes - D ': ['8'  , '8'                ],
+    '2 notes - E1': ['16' , 'r8' , '16'        ],
+    '2 notes - E2': ['8'  , 'r16', '16'        ],
+    '2 notes - E3': ['8.' , '16'               ],
+    '2 notes - F1': ['r16', '16' , 'r16', '16' ],
+    '2 notes - F2': ['r16', '8'  , '16'        ],
+
+    # 3 notes
+    '3 notes - A1': ['16' , '16' , '16' , 'r16'],
+    '3 notes - A2': ['16' , '16' , '8'         ],
+    '3 notes - B1': ['16' , 'r16', '16' , '16' ],
+    '3 notes - B2': ['8'  , '16' , '16'        ],
+    '3 notes - C1': ['16' , '16' , 'r16', '16' ],
+    '3 notes - C2': ['16' , '8'  , '16'        ],
+    '3 notes - D' : ['r16', '16' , '16' , '16' ],
+    
+    # 4 notes
+    '4 notes' : ['16' , '16', '16', '16'],
+}
+
+
+# Starting point to perform combinations
+readable_pattern = [
+
+    # 1 note
+    ['4'               ],
+    ['8'  , 'r8'       ],
+    ['r8' , '8'        ],
+    ['r16', '8.'       ],
+    ['r8' , 'r16', '16'],
+
+    # 2 notes
+    ['8.' , '16'],
+    ['r16', '16' , '8'],
+    ['16' , '8.'],
+    ['r8' , '16' , '16'],
+    ['r16', '8'  , '16'],
+    ['16' , '16' , '16' , 'r16'],
+    ['16' , '16' , '8'         ],
+    ['8'  , '16' , '16'        ],
+    ['16' , '8'  , '16'        ],
+    ['r16', '16' , '16' , '16' ],
+    
+    # 4 notes
+    ['16' , '16', '16', '16'],    
+]
 
 # Helper function
 def score(note_list, title=''):
@@ -16,6 +78,10 @@ def score(note_list, title=''):
     return sc.score([staff], title=title)
 
 
+#  ---------------------
+#  ALL POSSIBLE PATTERNS
+#  ---------------------
+
 # Create a sheet that will contain all patterns
 sheet = sc.sheet(title='Rythmic Patterns From 16th Notes')
 for k, v in pattern.items():
@@ -25,6 +91,31 @@ for k, v in pattern.items():
     n += [*v, *v, *v, 'r4' ]
     sheet.add_score(score(n, title='Pattern with {}'.format(k)))
 
+# Save it
+sheet.save(midi=False)
+
+
+#  -----------------
+#  TWO-WORDS PHRASES
+#  -----------------
+
+# Get a random list of N pair of random patterns
+N, iMin, iMax = 20, 0, len(readable_pattern)
+indices = np.random.randint(low=iMin, high=iMax, size=(20, 2))
+notes = []
+for index in indices:
+    i1, i2 = index[0], index[1]
+    w1, w2 = readable_pattern[i1], readable_pattern[i2] 
+    n  = [*w1, *w2, 'r2'] * 2
+    n += ['\\break']
+    n += [*w1, *w2, *w1, *w2] * 2
+    notes.append(n)
+
+# Create the sheet
+sheet = sc.sheet(title='Two-words Phrases From 16th-note Patterns')
+for i, n in enumerate(notes):
+    sheet.add_score(score(n, title='Phrase {}'.format(i)))
 
 # Save it
 sheet.save(midi=False)
+
