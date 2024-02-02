615be0256795c03adbef5c593213c85619303565
rmadar <romain@clratlpo06.in2p3.fr>
2024-02-02 17:24:13 +0100

    Improving layout for ninth chords printing

--- harmony/test_ninth.py
+++ harmony/test_ninth.py
@@ -3,15 +3,20 @@ sys.path.append("../src")
 import score_creator as sc
 import harmony as ha
 
-
+# Create the staff string
 str_staff = ''
-for nature in ha.possible_ninth:
-    C9 = ha.extended_chords.build(ha.note('c'), nature)
-    
-    str_staff += C9.lilypond_str()+'1'
+for i, nature in enumerate(ha.possible_ninth):
+    C9 = ha.extended_chords.build(ha.note('c'), nature)    
+    str_staff += C9.lilypond_str()+'1 \\bar ""'
+    if i==5: str_staff += '\\break'
 
+
+# Staff for chord names and for notes on treble key
 staff_chords = sc.staff(str_staff, clef='chords', time=None, tempo=None)
 staff_treble = sc.staff(str_staff, clef='treble', time=None, tempo=None)
+
+
+# Create the score and the final sheet
 score = sc.score([staff_chords, staff_treble])
 sheet = sc.sheet(score, title='Ninth Chords', composer='',
                  hide_BarNumber=True,
