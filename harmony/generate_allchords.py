b1f350f04123c32c55fe56661fa22064e20521b8
rmadar <romain@clratlpo06.in2p3.fr>
2024-05-31 00:44:15 +0200

    Updating all chords

--- harmony/generate_allchords.py
+++ harmony/generate_allchords.py
@@ -6,6 +6,8 @@ import harmony as ha
 # Initialized the staff string
 str_staff = ''
 
+Nchords = len(ha.possible_triads+ha.possible_tetrads+ha.possible_ninth+ha.possible_eleventh+ha.possible_thirteenth)
+
 # Triads
 for i, nature in enumerate(ha.possible_triads):
     C = ha.triad.build(ha.note('c'), nature)    
@@ -19,11 +21,11 @@ for i, nature in enumerate(ha.possible_tetrads):
     if i==6 : str_staff += '\\break'
 str_staff += '\\break'
     
-# Ninth chords
-for i, nature in enumerate(ha.possible_ninth):
-    C9 = ha.extended_chords.build(ha.note('c'), nature)    
-    str_staff += C9.lilypond_str()+'1 \\bar ""'
-    if i==5: str_staff += '\\break'
+# Extended chords (9th, 11th, 13th)
+for i, nature in enumerate(ha.possible_ninth+ha.possible_eleventh+ha.possible_thirteenth):
+    Cext = ha.extended_chords.build(ha.note('c'), nature)    
+    str_staff += Cext.lilypond_str()+'1 \\bar ""'
+    if i%5==0 and i>0: str_staff += '\\break'
 
 
 # Staff for chord names and for notes on treble key
@@ -33,7 +35,7 @@ staff_treble = sc.staff(str_staff, clef='treble', time=None, tempo=None)
 
 # Create the score and the final sheet
 score = sc.score([staff_chords, staff_treble])
-sheet = sc.sheet(score, title='All Chords', composer='',
+sheet = sc.sheet(score, title=f'All Chords - {Nchords} in total', composer='',
                  hide_BarNumber=True,
                  hide_TimeSignature=True)
 sheet.save(fname=f'all_chords.ly')
